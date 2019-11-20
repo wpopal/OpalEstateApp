f57684012e7f5f13caa332e1a525318a3079005d
Manh Cuong <dammanhcuong303@gmail.com>
2019-11-20 14:14:02 +0700

    convert searching

--- src/pages/Agency/Detail/style-detail.js
+++ src/pages/Agency/Detail/style-detail.js
@@ -1,5 +1,5 @@
 import {StyleSheet, Dimensions, Platform} from 'react-native';
-import {RFPercentage, RFValue} from "react-native-responsive-fontsize";
+import {RFPercentage, RFValue} from 'react-native-responsive-fontsize';
 
 const IS_IOS = Platform.OS === 'ios';
 const {width: viewportWidth, height: viewportHeight} = Dimensions.get('window');
@@ -19,6 +19,34 @@ export const itemWidth = slideWidth + itemHorizontalMargin * 2;
 const entryBorderRadius = 8;
 
 export default StyleSheet.create({
+  statust: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    justifyContent: 'space-between',
+    paddingLeft: '5%',
+    paddingRight: '5%',
+    paddingTop: '3%',
+  },
+  listing: {
+    width: (viewportWidth / 100) * 85,
+    height: (viewportHeight / 100) * 55,
+    marginBottom: 10,
+    borderRadius: 10,
+    marginTop: 10,
+    borderStyle: 'solid',
+    borderColor: '#fff',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 1.41,
+    elevation: 2,
+  },
   actionButtonIcon: {
     fontSize: 20,
     height: 22,
@@ -66,20 +94,4 @@ export default StyleSheet.create({
     fontWeight: 'bold',
     textAlign: 'center',
   },
-  listing: {
-    width: (viewportWidth / 100) * 90,
-    height: (viewportHeight / 100) * 28,
-    marginBottom : 100
-  },
-  statust: {
-    flex: 1,
-    width: '100%',
-    height: '100%',
-    flexDirection: 'row',
-    alignItems: 'flex-start',
-    justifyContent: 'space-between',
-    paddingLeft: '5%',
-    paddingRight: '5%',
-    paddingTop: '3%',
-  }
 });
